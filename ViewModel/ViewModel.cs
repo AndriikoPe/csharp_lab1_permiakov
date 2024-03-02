dc5d14cbd80e3a8ca3e908b40d7ad8998f95c2c3
AndriikoPe <andrusha20022002@gmail.com>
2024-03-02 09:38:17 +0200

    move main window to View folder, implement basic ViewModel

---
+++ ViewModel/ViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace permiakov_lab1.ViewModel
+{
+    internal class ViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ViewModel()
+        {
+        }
+
+        private void ExecuteCalculate(object parameter)
+        {
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
